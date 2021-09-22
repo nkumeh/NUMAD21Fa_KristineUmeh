6c9a8c22203a2218128246bbaa9a7307b04aea48
nkumeh <umeh.k@northeastern.edu>
2021-09-22 19:58:08 -0400

    setting up android manifest

--- app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity3.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity3.java
@@ -3,12 +3,29 @@ package edu.neu.madcourse.numad21fa_kristineumeh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button letter_a = findViewById(R.id.letter_a);
+        Button letter_b = findViewById(R.id.letter_b);
+        Button letter_c = findViewById(R.id.letter_c);
+        Button letter_d = findViewById(R.id.letter_d);
+        Button letter_e = findViewById(R.id.letter_e);
+        Button letter_f = findViewById(R.id.letter_f);
+
+        letter_a.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
