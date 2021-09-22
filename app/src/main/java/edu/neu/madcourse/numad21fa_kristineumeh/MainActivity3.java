60e9c1968fed13bee3c4924f594a90ba8d70a672
nkumeh <umeh.k@northeastern.edu>
2021-09-22 15:39:13 -0400

    creation of new activity

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity3.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21fa_kristineumeh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
