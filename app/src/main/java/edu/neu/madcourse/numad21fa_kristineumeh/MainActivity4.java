d1da4e3632cde614f2674fd9731f8770bc68c0b7
nkumeh <umeh.k@northeastern.edu>
2021-09-30 03:57:27 -0400

    link collector commit

--- app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity4.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity4.java
@@ -1,6 +1,7 @@
 package edu.neu.madcourse.numad21fa_kristineumeh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -11,21 +12,27 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    private ConstraintLayout constraintLayout;
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        //Declare the fab and assign
-        FloatingActionButton fab;
-
+        constraintLayout = findViewById(R.id.constraintLayout);
         fab = findViewById(R.id.fab1);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity4.this,"Click",Toast.LENGTH_SHORT).show();
+                snackBar();
             }
         });
+    }
+
+    public void snackBar() {
+        Snackbar snackbar = Snackbar.make(constraintLayout,"You have added a link",Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 }
