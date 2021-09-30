c5bd74c39acec36db1e3a761469a53e3f03deb50
nkumeh <umeh.k@northeastern.edu>
2021-09-30 03:00:17 -0400

    link collector commit

--- app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity4.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/MainActivity4.java
@@ -3,6 +3,9 @@ package edu.neu.madcourse.numad21fa_kristineumeh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        FloatingActionButton fab;
+
+        fab = findViewById(R.id.fab1);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
