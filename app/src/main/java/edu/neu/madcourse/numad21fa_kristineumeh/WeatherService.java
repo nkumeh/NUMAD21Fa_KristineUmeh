a2f692d3acda0fd268cf06951510d45af9e1c7bd
nkumeh <umeh.k@northeastern.edu>
2021-10-22 16:08:30 -0400

    Weather app optimisation

--- app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/WeatherService.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_kristineumeh/WeatherService.java
@@ -63,5 +63,5 @@ public class WeatherService extends AppCompatActivity {
 
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
-    }            }
+    }
 }
